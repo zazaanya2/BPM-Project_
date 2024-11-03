d90cc275103f17d831cf512fc3c37d8da173985e
Revalina Azahra <revalinaprinawan05@gmail.com>
2024-11-03 18:01:26 +0700

    table

--- bpm-front-end/src/main.jsx
+++ bpm-front-end/src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "@flaticon/flaticon-uicons/css/regular/rounded.css";
+import "@flaticon/flaticon-uicons/css/bold/rounded.css";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
