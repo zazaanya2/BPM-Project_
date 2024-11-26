da0c098b470f6c04a330d799a92e4c2bac293f59
Revalina Azahra <revalinaprinawan05@gmail.com>
2024-11-26 16:38:47 +0700

    app.jsx

--- bpm-front-end/src/App.jsx
+++ bpm-front-end/src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/backbone/Header'; 
-import Footer from './components/backbone/Footer';
-import Tentang from './components/page/2_Tentang/Root';
-import Berita from './components/page/3_Berita/Root';
-import RencanaKegiatan from './components/page/4_Kegiatan/Root'
-import Survei from './components/page/9_Survei/Root';
-import Pelaksanaan from './components/page/5_SPMI/siklus_spmi/pelaksanaan/Root';
-import Penetapan from './components/page/5_SPMI/siklus_spmi/penetapan/Root';
-import Peningkatan from './components/page/5_SPMI/siklus_spmi/peningkatan/Root';
-import Pengendalian from './components/page/5_SPMI/siklus_spmi/pengendalian/Root';
-import Evaluasi from './components/page/5_SPMI/siklus_spmi/evaluasi/Root';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/backbone/Header";
+import Footer from "./components/backbone/Footer";
+import Tentang from "./components/page/2_Tentang/Root";
+import Berita from "./components/page/3_Berita/Root";
+import RencanaKegiatan from "./components/page/4_Kegiatan/Root";
+import Survei from "./components/page/9_Survei/Root";
+import Pelaksanaan from "./components/page/5_SPMI/siklus_spmi/pelaksanaan/Root";
+import Penetapan from "./components/page/5_SPMI/siklus_spmi/penetapan/Root";
+import Peningkatan from "./components/page/5_SPMI/siklus_spmi/peningkatan/Root";
+import Pengendalian from "./components/page/5_SPMI/siklus_spmi/pengendalian/Root";
+import Evaluasi from "./components/page/5_SPMI/siklus_spmi/evaluasi/Root";
 import "./App.css";
 import Peraturan from "./components/page/10_Peraturan/ms_kebijakanPeraturan/Root";
 import PeraturanEksternal from "./components/page/10_Peraturan/ms_peraturanEksternal/Root";
@@ -25,11 +25,11 @@ function App() {
         {/* Header Component */}
         <Header />
 
-        <main className="flex-grow-1" >
-          <Tentang/>
-          <Berita/>
-          <RencanaKegiatan/>
-          <Survei/>
+        <main className="flex-grow-1">
+          <Tentang />
+          <Berita />
+          <RencanaKegiatan />
+          <Survei />
           <Pelaksanaan />
           <Penetapan />
           <Peningkatan />
@@ -38,6 +38,8 @@ function App() {
           <Peraturan />
           <PeraturanEksternal />
           <InstrumenAps />
+        </main>
+
         {/* Footer Component */}
         <Footer />
       </div>
