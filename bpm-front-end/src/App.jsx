f1486d820bca6c648c5b9796ec79d16fc22bbe1c
ZymboWannaCode <budiprasetiyo170@gmail.com>
2024-12-01 11:57:17 +0700

    merge

--- bpm-front-end/src/App.jsx
+++ bpm-front-end/src/App.jsx
@@ -1,27 +1,6 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/backbone/Header'; 
-import Footer from './components/backbone/Footer';
-import Tentang from './components/page/2_Tentang/Root';
-import Berita from './components/page/3_Berita/Root';
-import RencanaKegiatan from './components/page/4_Kegiatan/Root'
-import Survei from './components/page/9_Survei/Root';
-import Pelaksanaan from './components/page/5_SPMI/siklus_spmi/pelaksanaan/Root';
-import Penetapan from './components/page/5_SPMI/siklus_spmi/penetapan/Root';
-import Peningkatan from './components/page/5_SPMI/siklus_spmi/peningkatan/Root';
-import Pengendalian from './components/page/5_SPMI/siklus_spmi/pengendalian/Root';
-import Evaluasi from './components/page/5_SPMI/siklus_spmi/evaluasi/Root';
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/backbone/Header";
-import Footer from "./components/backbone/Footer";
-import Tentang from "./components/page/2_Tentang/Root";
-import Berita from "./components/page/3_Berita/Root";
-import RencanaKegiatan from "./components/page/4_Kegiatan/Root";
-import Survei from "./components/page/9_Survei/Root";
-import Peraturan from "./components/page/10_Peraturan/ms_kebijakanPeraturan/Root";
-import PeraturanEksternal from "./components/page/10_Peraturan/ms_peraturanEksternal/Root";
-import InstrumenAps from "./components/page/10_Peraturan/ms_instrumenAps/Root";
+
+
+
 
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -36,12 +15,13 @@ import Penetapan from "./components/page/5_SPMI/siklus_spmi/penetapan/Root";
 import Peningkatan from "./components/page/5_SPMI/siklus_spmi/peningkatan/Root";
 import Pengendalian from "./components/page/5_SPMI/siklus_spmi/pengendalian/Root";
 import Evaluasi from "./components/page/5_SPMI/siklus_spmi/evaluasi/Root";
-import "./App.css";
 import Peraturan from "./components/page/10_Peraturan/ms_kebijakanPeraturan/Root";
 import PeraturanEksternal from "./components/page/10_Peraturan/ms_peraturanEksternal/Root";
 import InstrumenAps from "./components/page/10_Peraturan/ms_instrumenAps/Root";
 import KriteriaSurvei from "./components/page/9_Survei/Kriteria_Survei/Root";
 import SkalaSurvei from "./components/page/9_Survei/Skala_Penilaian/Root";
+import RencanaKegiatan from "./components/page/4_Kegiatan/Root";
+import Survei from "./components/page/9_Survei/Root";
 
 function App() {
   return (
@@ -51,15 +31,11 @@ function App() {
       <div className="d-flex flex-column min-vh-100">
         {/* Header Component */}
         <Header />
-
-        <main className="flex-grow-1" >
-          <Tentang/>
-          <Berita/>
-          <RencanaKegiatan/>
-          <Survei/>
+          
         <main className="flex-grow-1">
           <Tentang />
           <Berita />
+          <RencanaKegiatan />
           <JadwalKegiatan />
           <DokumentasiKegiatan />
           <Pelaksanaan />
@@ -67,10 +43,6 @@ function App() {
           <Peningkatan />
           <Pengendalian />
           <Evaluasi />
-        <main className="flex-grow-1">
-          <Tentang />
-          <Berita />
-          <RencanaKegiatan />
           <Survei />
           <Peraturan />
           <PeraturanEksternal />
